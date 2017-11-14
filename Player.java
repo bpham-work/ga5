d70b47be9fc9abe9aef4bf66e61b88bd57ad26f9
Bryant Pham <bryantphamm@gmail.com>
2017-11-13 22:10:45 -0600

    init

---
+++ Player.java
@@ -0,0 +1,34 @@
+public class Player extends Actor {
+    public Player() {
+        super();
+        this.currRow = 4;
+        this.currCol = 4;
+    }
+
+    public void move(char input) {
+        int nextRow = this.currRow;
+        int nextCol = this.currCol;
+        switch (input) {
+            case 'w':
+                nextRow++;
+                break;
+            case 'a':
+                nextCol--;
+                break;
+            case 's':
+                nextRow--;
+                break;
+            case 'd':
+                nextCol++;
+                break;
+            default:
+                break;
+        }
+        if (areCoordsValid(nextRow, nextCol)) {
+            this.currRow = nextRow;
+            this.currCol = nextCol;
+        } else {
+            System.out.println("You cannot move off the map!");
+        }
+    }
+}
