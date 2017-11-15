75d2a3644b5f631a5ca537c661818ddb09f7ff85
Bryant Pham <bryantphamm@gmail.com>
2017-11-14 20:23:35 -0600

    Add doc

--- Actor.java
+++ Actor.java
@@ -8,6 +8,12 @@ public abstract class Actor {
             col >= 0 && col < 9;
     }
 
+    /**
+     * Move actor
+     *
+     * @param Character input used to determine direction of movement
+     * @return boolean denoting if movement was valid
+     */
     public abstract boolean move(Character input);
 
     public int getRow() {
